f63b3d2b020ad22123b92251cc4e40cc37acaf95
ritz <j.ritz@fz-juelich.de>
2020-05-05 11:12:00 +0200

    Längenberechnung von dem,was durch den Pixel ge

--- xrtomo_main.py
+++ xrtomo_main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+eps = np.finfo(np.float32).eps
 unit_square = np.array([[-0.5, 0.5], [-0.5, 0.5]])
-ng = 10
+ng = 15
 xs = np.linspace(unit_square[0,0]+1/(2*ng), unit_square[0,1]-1/(2*ng), ng)
 ys = np.linspace(unit_square[1,0]+1/(2*ng), unit_square[1,1]-1/(2*ng), ng)
-x_y_d = []
+x_y_d = [] #coordinates of pixel middle and density
 
 for x in xs:
     for y in ys:
@@ -18,31 +19,47 @@ ns = 10 # Anzahl der Strahlen
 nw = 10 # Anzahl der Drehwinkel
 
 
-def get_cut_pixel(startpkt, endpunkt, x_y_d): # gibt die pixel an, die von der Gerade geschnitten werden
+def get_cut_pixel(startpkt, endpkt, x_y_d): # gibt die pixel an, die von der Gerade geschnitten werden
     cut_pixel = []
     for pixel in x_y_d:
         rectangle = np.array([[pixel[0] - cell_width / 2, pixel[1] - cell_height / 2],
                               [pixel[0] - cell_width / 2, pixel[1] + cell_height / 2],
                               [pixel[0] + cell_width / 2, pixel[1] + cell_height / 2],
                               [pixel[0] + cell_width / 2, pixel[1] - cell_height / 2]])
-        if( line_intersect(startpkt, endpunkt, rectangle[0], rectangle[1]) \
-               or line_intersect(startpkt, endpunkt, rectangle[1], rectangle[2]) \
-               or line_intersect(startpkt, endpunkt, rectangle[2], rectangle[3]) \
-               or line_intersect(startpkt, endpunkt, rectangle[3], rectangle[0])):
-            cut_pixel.append(pixel)
+        schnittpunkte = []
+        min_x = rectangle[0][0]-eps
+        max_x = rectangle[2][0]+eps
+        min_y = rectangle[0][1]-eps
+        max_y = rectangle[1][1]+eps
+        for i in range(4):
+            tmp = seg_intersect(rectangle[i], rectangle[(i+1)%4], startpkt, endpkt)
+            tmp_sp = [list(item) for item in schnittpunkte]
+            if type(tmp) != type(None) and min_x <= tmp[0] <= max_x and min_y <= tmp[1] <= max_y and list(tmp) not in tmp_sp:
+                schnittpunkte.append(tmp)
+        if len(schnittpunkte) >= 2:
+            len_of_cut = np.linalg.norm(schnittpunkte[0]-schnittpunkte[1])
+            if len_of_cut > eps:
+                cut_pixel.append((pixel, len_of_cut))
     return cut_pixel
 
-def line_intersect(start_1, ende_1, start_2, ende_2):
-    q = (start_1[1] - start_2[1]) * (ende_2[0]-start_2[0]) - (start_1[0]-start_2[0])  * (ende_2[1]-start_2[1])
-    d = (ende_1[0] - start_1[0]) * (ende_2[1]-start_2[1]) - (ende_1[1]-start_1[1])  * (ende_2[0]-start_2[0])
-    if (d == 0):
-        return False
-    r = q / d
-    q = (start_1[1] - start_2[1]) * (ende_1[0] - start_1[0]) - (start_1[0] - start_2[0]) * (ende_1[1] - start_1[1])
-    s = q / d
-    if (r < 0 or r > 1 or s < 0 or s > 1):
-        return False
-    return True
+def seg_intersect(a1, a2, b1, b2) :
+    da = a2-a1
+    db = b2-b1
+    dp = a1-b1
+    dap = perp(da)
+    denom = np.dot( dap, db)
+    num = np.dot( dap, dp )
+    eps = 0.00000000000000001
+    if np.abs(denom.astype(float)) < eps:
+        return None
+    else:
+        return (num / denom.astype(float))*db + b1
+
+def perp( a ) :
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
 
 def plot(startpunkt, endpunkt):
     plt.plot([-0.5, -0.5, 0.5, 0.5, -0.5], [-0.5, 0.5, 0.5, -0.5, -0.5], 'black')
@@ -61,12 +78,13 @@ def gitter_plot():
 
 
 if __name__ == "__main__":
-    startpunkt = np.array([-0.5, -0.5])
-    endpunkt = np.array([0.5, 0.5])
+    startpunkt = np.array([-1, -1])
+    endpunkt = np.array([1.2, 1.3])
     plot(startpunkt, endpunkt)
     cut_pixel = get_cut_pixel(startpunkt, endpunkt, x_y_d)
     for elem in cut_pixel:
-        plt.plot([elem[0]], [elem[1]], 'or')
+        plt.plot([elem[0][0]], [elem[0][1]], 'or')
+        print(elem)
     plt.show()
 
 
